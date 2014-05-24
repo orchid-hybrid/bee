eb0610791e217c1de71ddcf7c1b372c0f58e0ec8
orchid-hybrid <orchid-hybrid@users.noreply.github.com>
2014-05-24 04:38:45 +0100

    Create test.c

---
+++ test.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int f(int a, int b, int c, int d, int e);
+int g(int a, int b, int c, int d, int e);
+
+int main(void) {
+  int i;
+  int a,b,c,d,e;
+  int v1, v2;
+  
+  srand(time(0));
+  
+  for(i = 0; i < 100; i++) {
+    a = rand()%5000-1000;
+    b = rand()%5000-1000;
+    c = rand()%5000-1000;
+    d = rand()%5000-1000;
+    e = rand()%5000-1000;
+    
+    printf("%d %d %d %d %d\n",a,b,c,d,e);
+    v1=f(a,b,c,d,e);
+    printf("%d\n", v1);
+    v2=g(a,b,c,d,e);
+    printf("%d\n", v2);
+    if(v1 != v2) {
+      return EXIT_FAILURE;
+    }
+  }
+  
+  return EXIT_SUCCESS;
+}
